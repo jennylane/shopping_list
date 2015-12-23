f3a1bf066f399416e9fbd51798a7836ecdd214b1
jennylane <jenny.lane@blackbaud.com>
2015-12-23 09:32:15 -0700

    final

--- js/app.js
+++ js/app.js
@@ -1,6 +1,24 @@
 $(document).ready(function() {
+	
+//click Add button adds textbox value to the right and clears textbox
 	$('.addbutton').click(function() {
-	var userItem= '<div class="userItem">'+ $('input.addbox').val() +'</div>';
-	$('.listcontainer').append(userItem);
+		var userItem= '<div class="userItem">'+ $('input.addbox').val() +'</div>';
+		$('.listcontainer').append(userItem);
+		$('.addbox').val('')
 	});
+
+//click a div inside of .listcontainer toggles strike class
+	$('.listcontainer').on('click', 'div', function() {
+  		$(this).toggleClass('strike');
 	});
+
+//click the delete text removes strike through items from DOM
+	$('.delete').click(function() {
+		$('.strike').remove();
+	});
+
+//click delete all text removes all userItem
+	$('.deleteAll').click(function() {
+		$('.userItem').remove();
+		});
+});
